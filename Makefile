5edcbc7b0d8cd8d5a08bef025080c34c8969bc27
mayr0y <alexlzrw@gmail.com>
2023-07-02 23:34:13 +0300

    complete task1

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 PHONY: generate
 generate:
-			protoc 	--proto_path api/note_v1	\
-			--go_out=pkg/note_v1 --go_opt=paths=source_relative \
-			--go-grpc_out=pkg/note_v1 --go-grpc_opt=paths=source_relative \
-			api/note_v1/note.proto
+					mkdir -p pkg/note_v1
+					protoc 	--proto_path api/note_v1	\
+                    			--go_out=pkg/note_v1 --go_opt=paths=source_relative \
+                    			--go-grpc_out=pkg/note_v1 --go-grpc_opt=paths=source_relative \
+                    			api/note_v1/note.proto
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,26 @@
 package app
+
+import (
+	"github.com/alexlzrv/note-service/internal/app/api/note_v1"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+
+	desc "github.com/alexlzrv/note-service/pkg/note_v1"
+)
+
+const port = ":50051"
+
+func InitServer() {
+	list, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to mapping port: %s", err.Error())
+	}
+
+	s := grpc.NewServer()
+	desc.RegisterNoteV1Server(s, note_v1.NewNote())
+
+	if err = s.Serve(list); err != nil {
+		log.Fatalf("Failed to serve: %s", err.Error())
+	}
+}
